9f17c1071ee965c4e3cc0f543afa5fee25c1dd80
fipsher <fipsher@gmail.com>
2016-03-24 18:30:20 +0200

    Update AvailableCarsForm.cs

--- CarRentalManager/Forms/AvailableCarsForm.cs
+++ CarRentalManager/Forms/AvailableCarsForm.cs
@@ -26,12 +26,14 @@ namespace CarRentalManager.Forms
         }
 
         private void btnAccept_Click(object sender, EventArgs e)
-        {               
+        { 
+             //Rewiew AZ: can we remove logic outside Form calss? (Recoment to create additional class library with business logic (it will helps you with Unit Tests + exeption handling))
+            //you can even add some exeption hangling and base logging
             string dateStart = dtpStart.Value.ToString("yyyy-MM-dd");
             string dateEnd = dtpEnd.Value.ToString("yyy-MM-dd");
             string carClass = (cbCarClass.SelectedItem == null)? "": cbCarClass.SelectedItem.ToString();
             int passengerNum = tbPassengerNum.Text.ToString() == "" ? 0 : int.Parse(tbPassengerNum.Text.ToString());
-            decimal fuelConsumption = tbFuelConsuption.Text.ToString() == ""? 90: decimal.Parse(tbFuelConsuption.Text.ToString());
+            decimal fuelConsumption = tbFuelConsuption.Text.ToString() == ""? 90: decimal.Parse(tbFuelConsuption.Text.ToString());// if i input number(or another wrong data) with dott it throws exception
             decimal carPrice = tbCarPrice.Text.ToString() == ""? 10000: decimal.Parse(tbCarPrice.Text.ToString());
 
             List<Car> carsList = _carRepository.GetAvailableCars(dateStart, dateEnd, carClass, 
